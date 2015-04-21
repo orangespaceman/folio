3355df4dfb01677f8684453c54af9590d14ed4d4
Pete Goodman <thegingerbloke@users.noreply.github.com>
2015-04-21 16:26:37 +0100

    Nicer JS lint error messages

--- gulp-tasks/lint-js-jscs.js
+++ gulp-tasks/lint-js-jscs.js
@@ -5,11 +5,21 @@
 
 var gulp = require('gulp');
 var jscs = require('gulp-jscs');
+var map = require('map-stream');
+var gutil = require('gulp-util');
 
 module.exports = function () {
 
-    return gulp
+    var stream = gulp
         .src(global.config.jsFiles)
         .pipe(jscs());
+
+    stream.on('error', function (err) {
+        console.log(err.message);
+        stream.emit('end');
+        gutil.beep();
+    });
+
+    return stream;
 
 };

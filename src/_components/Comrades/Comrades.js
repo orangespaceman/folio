954786a16d07c1e0c3363ab630f505eed67d0f3a
Pete Goodman <thegingerbloke@users.noreply.github.com>
2017-03-20 13:01:26 +0000

    Add Comrades filter

---
+++ src/_components/Comrades/Comrades.js
@@ -0,0 +1,45 @@
+/*
+ * Comrade filter
+ */
+
+'use strict'
+
+var comradeFilter
+var comradeListItems
+
+function init () {
+  comradeFilter = document.querySelector('.js-Comrades-filter')
+  if (!comradeFilter) return
+
+  comradeListItems = document.querySelectorAll('.js-Comrades-item')
+
+  comradeFilter.classList.remove('u-hidden')
+  comradeFilter.addEventListener('input', onInputChange)
+}
+
+function onInputChange (e) {
+  var searchValue = e.target.value.toLowerCase()
+  if (!searchValue) {
+    Array.prototype.forEach.call(comradeListItems, setActiveState)
+  } else {
+    Array.prototype.forEach.call(comradeListItems, function (comradeListItem) {
+      if (comradeListItem.dataset.skills.toLowerCase().indexOf(searchValue) > -1 || comradeListItem.textContent.toLowerCase().indexOf(searchValue) > -1) {
+        setActiveState(comradeListItem)
+      } else {
+        setInactiveState(comradeListItem)
+      }
+    })
+  }
+}
+
+function setActiveState (el) {
+  el.classList.remove('is-inactive')
+}
+
+function setInactiveState (el) {
+  el.classList.add('is-inactive')
+}
+
+module.exports = {
+  init: init
+}
--- src/_js/site.js
+++ src/_js/site.js
@@ -5,10 +5,12 @@
 var lazyload = require('./lazyload-images')
 var consoleMessage = require('./console')
 var relExternal = require('./rel-external')
+var comrades = require('../_components/Comrades/Comrades')
 var feeds = require('../_components/Feed/Feeds')
 
 lazyload.init()
 consoleMessage.init()
+comrades.init()
 relExternal.init()
 feeds.init({
   host: 'api.petegoodman.com',
